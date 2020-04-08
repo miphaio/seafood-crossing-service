f7710ac3f18a99653dd0146d786626ed8764360f
WMXPY <wm@mengw.io>
2020-04-08 12:57:44 -0500

    create destination categories

---
+++ src/declare/destination.ts
@@ -0,0 +1,14 @@
+/**
+ * @author WMXPY
+ * @namespace Declare
+ * @description Destination
+ */
+
+export enum DESTINATION_CATEGORY {
+
+    TURNIP_BUY = "TURNIP_BUY",
+    TURNIP_SELL = "TURNIP_SELL",
+    TOUR = "TOUR",
+    SAHARAH = "SAHARAH",
+    OTHER = "OTHER",
+}
