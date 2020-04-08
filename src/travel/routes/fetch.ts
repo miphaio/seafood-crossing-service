d7333ec77d187f7f7647dd5dc4e57e3a1ec7bf3e
WMXPY <wm@mengw.io>
2020-04-08 17:20:30 -0500

    add destination category to response

--- src/travel/routes/fetch.ts
+++ src/travel/routes/fetch.ts
@@ -4,6 +4,7 @@
  * @description Fetch
  */
 
+import { DESTINATION_CATEGORY } from "../../declare/destination";
 import { DestinationModel } from "../../model/destination";
 import { fetchActiveAvailableDestinations } from "../../service/destination";
 
@@ -11,6 +12,8 @@ export type FetchTravelDestinationElement = {
 
     readonly destinationId: string;
     readonly accountId: string;
+
+    readonly category: DESTINATION_CATEGORY;
 
     readonly title: string;
     readonly description: string;
@@ -26,9 +29,11 @@ export const fetchTravelDestinationRoute = async (): Promise<Record<string, any>
     const mapped: FetchTravelDestinationElement[] = [];
 
     for (const destination of destinations) {
+
         mapped.push({
             destinationId: destination._id.toString(),
             accountId: destination._account.toHexString(),
+            category: destination.category,
             title: destination.title,
             description: destination.description,
             occupanciesLength: destination.occupancies.length,
